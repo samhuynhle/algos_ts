9071d1178af9025c9c14ed1bdf86fc98d0e2782a
Sam Le <samhuynhle@gmail.com>
2020-10-27 20:43:02 -0700

    added notes to quick sort

--- sortedSquares.ts
+++ sortedSquares.ts
@@ -46,3 +46,27 @@ function quickSort(A: number[], left: number = 0, right: number = A.length - 1) 
 
     return A
 }
+
+/*
+What is Quick Sort?
+- Quick sort is a divide and conquer algorithm, creating
+-- two empy arrays to hold elements less than the pivot and greater than the pivot value.
+-- There are two basic operations in the algorithm, swapping items in place and
+-- partitioning a section of the array.
+
+Steps
+- Find a "pivot" item in the array, this is done for the getting a starting point for comparison
+- Start a pointer (the left) at the first item in the array
+- Start a pointer (the right) at the last item in the array
+- while the value at the left points in the array is less than 
+-- the pivot value, move the left pointer to the right (adding 1). Continue to until the 
+-- value at the left pointer is greater than or equal to the pivot value.
+- While the value at the right pointer is greater than the pivot value, move the right pointer
+-- to the left (subtracting 1). Continue until the value at the right pointer is less
+-- than or equal to teh pivot value.
+- If the left pointer is less than or equal to the right pointer, then swap the
+-- the values at these locations in the array
+- Move the left pointer to the right by one and the right pointer to the left by one
+- If the left pointer and right pointer don't meet, go to step 1
+
+*/
